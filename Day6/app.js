41704115fddb40c4f902a22a11b34627517cf69a
Nguyễn Danh Phương <nguyendanhphuong0305@gmail.com>
2023-03-18 09:50:33 +0700

    new commit

---
+++ Day6/app.js
@@ -0,0 +1,18 @@
+// 1. Kiểu Giá trị ở trong Javascript:
+// - String(Chuỗi) ví dụ "Tên của em", "123"
+// - Number(Số) ví dụ: 100, 200
+// - Boolean(Đúng hoặc sai) ví dụ true/false
+// - undefined
+// - null
+// console.log("Hello Trâm");
+// console.log(123);
+
+// 2. Khai báo giá trị với let, const:
+let name = "Trâm";
+name = "Phương";
+let age = 15;
+let text = "Tôi tên là " + name + ", năm nay tôi " + age + " tuổi";
+
+console.log(name);
+console.log(age);
+console.log("Tôi tên là " + name + ", năm nay tôi " + age + " tuổi");
